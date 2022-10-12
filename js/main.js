b54c55b6a69bdda46291251fee6ada444f0582a1
ansonsow <60858599+ansonsow@users.noreply.github.com>
2022-10-12 14:31:51 -0700

    example of reading db info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-import * as db from "./app.js"
-// example of call the db function outside of app.js
-// db.writeUserData("a","fa","la","amail","[1,2]","aaa.aa");
+import * as ldb from "./app.js"
+
+import {Firestore, query, getFirestore, collection, doc, updateDoc, getDocs,getDoc, addDoc, deleteDoc, arrayUnion, arrayRemove} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js'
+const db = getFirestore();
+
+// example of reading info from db
+const q = query(collection(db, "events"));
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+});
+
+// example of call the db function outside of appjs
+// ldb.writeUserData("a","fa","la","amail","[1,2]","aaa.aa");
 
 $(document).ready(function(){
     /*------ HOME SLICK CAROUSEL ------*/
