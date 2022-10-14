adc8721d2b2745976caa85f0a3d5ba72a1c99c54
HT <rhizodont@gmail.com>
2022-10-13 23:22:56 -0700

    fixed hamburger click + new js file to call FB

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-// import * as dbf from "./app.js"
-
-// import {Firestore, query, getFirestore, collection, doc, getDocs,getDoc} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js'
-// const db = getFirestore();
-
-// example of reading info from db
-
-// all documents in a collection
-// const q = query(collection(db, "events"));
-// const querySnapshot = await getDocs(q);
-// querySnapshot.forEach((doc) => {
-//     console.log(doc.data());
-// });
-
-// single document
-// const querySnapshot2 = await getDoc(doc(db, "events","uGfj5SGWqdBIdFsM7Lie"));
-// console.log(querySnapshot2.data());
-
-// single field
-// const querySnapshot2 = await getDoc(doc(db, "events","uGfj5SGWqdBIdFsM7Lie"));
-// console.log(querySnapshot2.data().price);
-
-// example of call the db function outside of appjs
-// dbf.writeUserData("a","fa","la","amail","[1,2]","aaa.aa");
-
 $(document).ready(function(){
     /*------ HOME SLICK CAROUSEL ------*/
     $(".home-carousel").slick({
