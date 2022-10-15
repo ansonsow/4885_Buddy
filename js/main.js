fc01e2412934e95f89b5da235a238627c2621f7a
HT <rhizodont@gmail.com>
2022-10-14 17:50:42 -0700

    added topbar & footer as reusable parts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+/*------ INCLUDE HTML ------*/
+/* credit: www.w3schools.com/howto/tryit.asp?filename=tryhow_html_include_2 */
+function jsIncludeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("js-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("js-include-html");
+          elmnt.replaceWith(...elmnt.childNodes); // unwrap
+          jsIncludeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
+jsIncludeHTML();
+
 $(document).ready(function(){
     /*------ HOME SLICK CAROUSEL ------*/
     $(".home-carousel").slick({
