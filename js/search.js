e35449ae85a1d412194cf6fee6421e4ee933260f
ansonsow <60858599+ansonsow@users.noreply.github.com>
2022-10-24 15:34:08 -0700

    started search js

---
+++ js/search.js
@@ -0,0 +1,11 @@
+// import * as dbf from "./app.js"
+// // import * as mapf from "./tomtom.js"
+// import {query, collection, doc, getDocs,getDoc,where} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js'
+
+
+console.log("aaa");
+// let e = query(collection(dbf.db, "events"));
+//     const starSnapshot = await getDocs(e);
+//     starSnapshot.forEach((doc)=>{
+//      console.log(doc.coverImage);
+//     })
