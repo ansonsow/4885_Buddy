89162377c231367d8a6ef3584d35886581c7866f
HT <rhizodont@gmail.com>
2022-10-25 14:41:00 -0700

    manageMyEvents – arrow rework in progress

--- js/manageMyEvents.js
+++ js/manageMyEvents.js
@@ -83,8 +83,6 @@ if(currentUser){
     $(document).ready(function(){
         $(".event-block[hidden]").before("<div dummy-area></div>")
 
-        /*------ EVENTS ------*/
-
         /*------ RANDOM CAT IMAGES ------*/
         // delete later, just for cloning purposes
         for(let i=0; i<6; i++){
@@ -101,7 +99,9 @@ if(currentUser){
 
         $(".event-block[hidden]").remove();
 
-        /*------------------------------------*/
+        /********************************************************************** */
+        /******* IMPORTANT REUSABLE VARIABLES ********************************* */
+        /********************************************************************** */
 
         // $(".event-block:first").attr("event-id",oopsie)
 
@@ -112,107 +112,138 @@ if(currentUser){
         let slidesNeeded = Math.ceil(eventsCount/eventsPerRow);
         let panelSpan = "(var(--Events-Grid-Width) + var(--Events-Spacing))";
 
-        // label each column with its number
-        // column 1,
-        // column 2,
-        // column 3.
-        for(let i=0; i<=eventsPerRow; i++){
-            $(`.events-grid .event-block:nth-of-type(${eventsPerRow}n+${i})`).attr("column",i);
-        }
+        /********************************************************************** */
+        /******* FUNCTION: GENERATE COLUMN#S & SLIDE#S ************************ */
+        /********************************************************************** */
+        // ideally used for afte the trash icon is pressed
 
-        let eventBlockJQ = $(".events-grid > .event-block");
-
-        for(let i=0; i<eventBlockJQ.length; i+=eventsPerRow){
-            eventBlockJQ.slice(i, i+eventsPerRow).attr("slide",i) // gives 0, 3, 6, 9
-            // eventBlockJQ.slice(i, i+eventsPerRow).attr("slide",Math.random().toString(36).slice(2,7))
-        }
-
-        // revamp the slide numbers
-        $(".event-block[slide]").each(function(){
-            let numAttr = Number($(this).attr("slide"));
-
-            // if it's 0, make it 1 since it's gonna be the 1st slide
-            if(numAttr == 0){
-                $(this).attr("slide","1");
+        function reInitEvents(){
+            // label each column with its number
+            // column 1,
+            // column 2,
+            // column 3.
+            for(let i=0; i<=eventsPerRow; i++){
+                $(`.events-grid .event-block:nth-of-type(${eventsPerRow}n+${i})`).attr("column",i);
             }
-            
-            // if it's anything larger than 1, divide it by eventsPerRow,
-            // then +1 because there's already a "slide 1" (created from "0")
-            else if(numAttr > 0){
-                let newnum = (numAttr/3)+1;
-                $(this).attr("slide",newnum)
+
+            let eventBlockJQ = $(".events-grid > .event-block");
+
+            for(let i=0; i<eventBlockJQ.length; i+=eventsPerRow){
+                eventBlockJQ.slice(i, i+eventsPerRow).attr("slide",i) // gives 0, 3, 6, 9 (assuming 3 cols)
             }
-        })
+
+            // revamp the slide numbers
+            // from e.g. slide 0, 3, 6, 9 --> slide 1, 2, 3, 4
+            $(".event-block[slide]").each(function(){
+                let numAttr = Number($(this).attr("slide"));
+
+                // if it's 0, make it 1 since it's gonna be the 1st slide
+                if(numAttr == 0){
+                    $(this).attr("slide","1");
+                }
+                
+                // if it's anything larger than 1, divide it by eventsPerRow,
+                // then +1 because there's already a "slide 1" (created from "0")
+                else if(numAttr > 0){
+                    let newnum = (numAttr/eventsPerRow)+1;
+                    $(this).attr("slide",newnum)
+                }
+            })
+        }//end reInitEvents()
+
+        reInitEvents();        
 
         /*--------------------*/
     
         $(".events-grid").attr("slides-needed",slidesNeeded);
         $(".events-grid").attr("style","--Events-Per-Row:" + eventsCount);
     
-        // first PANEL
+        /********************************************************************** */
+        /******* 1ST PANEL || INITIAL VIEW ************************************ */
+        /********************************************************************** */
+
+        let panelView = 1;
         $(".events-grid").attr("panel-view","1");
         $(".prev-events").css("visibility","hidden");
     
         if(slidesNeeded == 1){
-            $(".next-events").css("visibility","hidden");
+            $(".prev-events, .next-events").css("visibility","hidden");
         }
     
+        /********************************************************************** */
+        /******* SLIDESHOW ARROW CLICK **************************************** */
+        /********************************************************************** */
         $(".events-arrow").click(function(){
-            // keep track of which panel user is currently viewing
-            let whichPanel = Number($(".events-grid").attr("panel-view"));
     
-            /*------ CLICK NEXT ------*/
+            /****************************************************************** */
+            /******* NEXT ARROW CLICK ***************************************** */
+            /****************************************************************** */
             if($(this).hasClass("next-events")){
+                
+                $(".prev-events").css("visibility","");
+
                 // if there are only 2 panels
                 if(slidesNeeded == 2){
                     $(".prev-events").css("visibility","");
                 }
-    
-                // press-next: if user CAN click next
-                if(whichPanel < slidesNeeded-1){
-                    whichPanel += 1;
-                    $(".prev-events").css("visibility","");
-                    $(".events-grid").attr("panel-view",whichPanel);
-    
-                    $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")")
-                }
-    
-                // press-next: if user can't go any further
+
+                // if there are MORE than 2 panels
+                // MOVE TO NEXT PANEL
                 else {
-                    whichPanel += 1;
-                    $(".events-grid").attr("panel-view",whichPanel);
-                    $(".next-events").css("visibility","hidden");
-                    $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")")
+                    panelView += 1;
+
+                    // if user is at the END, can't click right
+                    if(panelView >= slidesNeeded){
+                        panelView = slidesNeeded;
+                        $(".events-grid").attr("panel-view",panelView);
+                        $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(panelView-1) + ")");
+                        $(".next-events").css("visibility","hidden");
+                    }
+                    
+                    // if user CAN keep clicking right
+                    else {
+                        $(".events-grid").attr("panel-view",panelView);
+                        $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(panelView-1) + ")")
+                    }
+                    
                 }
             }
     
-            /*------ CLICK PREV ------*/
+            /****************************************************************** */
+            /******* PREV ARROW CLICK ***************************************** */
+            /****************************************************************** */
             else if($(this).hasClass("prev-events")){
-                // press-prev: if user can't go left any further
-                if(whichPanel == 2){
-                    whichPanel == 1;
+                // if there's only 1 slide, remove prev arrow
+                if(slidesNeeded <= 1){
+                    panelView = 1;
                     $(".events-grid").attr("panel-view","1");
                     $(".prev-events").css("visibility","hidden");
-                    $(".events-grid").css("margin-left","0");
-                    $(".next-events").css("visibility","");
+                    $(".events-grid").css("margin-left","");
                 }
-    
-                // press-prev: if user CAN go prev
-                else if(whichPanel > 2){
-                    whichPanel -= 1;
-                    $(".events-grid").attr("panel-view",whichPanel);
-                    $(".prev-events").css("visibility","");
-                    $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")");
-    
-                    // second-last slide
-                    if(whichPanel < slidesNeeded){
-                        $(".next-events").css("visibility","");
+
+                // if there's more than 1 slide, show the prev arrow
+                else if(slidesNeeded > 1){
+                    panelView -= 1;
+                    $(".events-grid").attr("panel-view",panelView);
+                    $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(panelView-1) + ")");
+
+                    // if user CAN keep pressing prev
+                    if(panelView > 1){
+                        $(".prev-events").css("visibility","");
                     }
+                    
+                    // if user CAN'T keep pressing prev
+                    else {
+                        $(".prev-events").css("visibility","hidden");
+                    }
+                    
                 }
             }
         })//end arrows click
     
-        /*------ TRASH: CLICK ------*/
+        /********************************************************************** */
+        /******* TRASH ICON CLICK ********************************************* */
+        /********************************************************************** */
         let rmEVspeed = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--Remove-Event-Speed"));
     
         $(document).on("click", ".delete-event", function(){
@@ -232,68 +263,6 @@ if(currentUser){
     
                     // remove DELETED EVENT
                     $(that).parents(".event-block").remove();
-    
-                    // reinitialize values
-                    eventsCount = $(".event-block").length;
-                    slidesNeeded = Math.ceil(eventsCount/eventsPerRow);
-    
-                    $(".events-grid").attr("slides-needed",slidesNeeded);
-                    $(".events-grid").attr("style","--Events-Per-Row:" + eventsCount);
-
-                    // next line works
-                    // alert(`ORIGINAL ITEMS: ${originalEventsNum} || CURRENT ITEMS: ${eventsCount} || SLIDES NEEDED: ${slidesNeeded}`);
-
-                    //
-    
-                    // IF THERE IS ONLY 1 SLIDE LEFT (aka 3 events)
-                    if(slidesNeeded == 1){
-                        whichPanel = slidesNeeded;
-                        $(".prev-events, .next-events").css("visibility","hidden");
-                        $(".events-grid").css("margin-left","0");
-                        $(".events-grid").attr("panel-view","1");
-                        
-                        // $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")");
-                    }
-                    
-                    // IF THERE ARE *MORE* THAN 1 SLIDE(S) LEFT
-                    else if(slidesNeeded > 1){
-
-                        // condition A: YOU ARE ON THE LAST POSSIBLE SLIDE
-                        // condition B: you click 🗑️ on one of the last items
-                        if(slidesNeeded == whichPanel){
-                            // alert(`you are STILL on slide ${whichPanel}`)
-                            // alert("you are on the LAST SLIDE, which is ALSO WHERE YOU PRESSED THE DELETE");
-
-                            $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")");
-                        }
-                        
-                        // if you are:
-                        // NOT on the last possible slide, e.g. slide 2 of 3
-                        // you click one of those 🗑️
-                        else {
-                            // whichPanel -= 1;
-
-                            // alert(whichPanel)
-
-                            // next 2 lines are correct
-                            $(".events-grid").attr("panel-view",whichPanel);
-                            $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")")
-
-
-
-                            // $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")");
-                        }
-                        
-
-                        // whichPanel = slidesNeeded;
-
-                        // $(".events-grid").attr("panel-view",slidesNeeded);
-                        // alert(whichPanel)
-
-                        // $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")");
-                    } else if(slidesNeeded == 3){
-                        // $(".events-grid").css("margin-left","calc(" + panelSpan + " * -" + Math.floor(whichPanel-1) + ")");
-                    }
                 },rmEVspeed)
             },rmEVspeed)        
         })
