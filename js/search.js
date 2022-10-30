48e7b399e2b29cc08f542823f0a0295e2bb17a66
ansonsow <60858599+ansonsow@users.noreply.github.com>
2022-10-30 16:05:00 -0700

    search – tagsearch and reset button

--- js/search.js
+++ js/search.js
@@ -4,34 +4,14 @@ import {query, collection, doc, getDocs,getDoc,where} from 'https://www.gstatic.
 
 
 // console.log("aaa");
+let allSearch = [];
+
 let e = query(collection(dbf.db, "events"));
 const starSnapshot = await getDocs(e);
 starSnapshot.forEach((doc)=>{
     displayResult(doc.data());
-    // console.log(doc.data().coverImage);
-    // let eventBlock = document.querySelector(".event-container");
-    // let clonedEvent = eventBlock.cloneNode(true);
-    // clonedEvent.removeAttribute("hidden");
-    // // document.getElementById("result").appendChild(eventBlock)
-    // eventBlock.parentNode.insertBefore(clonedEvent, eventBlock.nextSibling);
-    // clonedEvent.querySelector(".card-content.image.search-page").src = doc.data().coverImage;
-    // clonedEvent.querySelector(".card-content.event-name").innerHTML = doc.data().name;
-    // // clonedEvent.querySelector(".card-content.location").innerHTML = doc.data().name;
+    allSearch.push(doc.data())
 
-    // function reverseGeo(l){
-    //     tt.services.reverseGeocode({
-    //     key: tomtomApiKey,
-    //     position: l
-    // })
-    // .then(result=>{
-    //     clonedEvent.querySelector(".card-content.location").innerHTML = result.addresses[0].address.freeformAddress
-    //     // console.log(result.addresses[0].address.freeformAddress);
-    // });
-    // }
-    // reverseGeo(doc.data().location)
-
-    // // card-content location
-    // clonedEvent.querySelector(".card-content.time").innerHTML = "not sure yet";
 })
 
 
@@ -53,7 +33,6 @@ function displayResult(doc){
         } else {
             if(typeof jQuery !== "undefined"){
                 clearInterval(load_jQuery);
-
                 document.querySelector(".cloned-events-container").append(clonedEvent);
             }		
         }
@@ -79,10 +58,9 @@ function displayResult(doc){
 
 }
 
+let searchResult = [];
 
 document.getElementById("searchButton").addEventListener("click",async()=>{
-    let allSearch = [];
-    let searchResult = [];
     document.querySelectorAll(".cloned-events-container").forEach(event => {
         event.replaceChildren();
     });
@@ -90,71 +68,64 @@ document.getElementById("searchButton").addEventListener("click",async()=>{
     const searchSnapshot = await getDocs(e);
     searchSnapshot.forEach((doc)=>{
         // console.log(doc.data());
-        allSearch.push(doc.data())
     })
     console.log(allSearch);
     let textSearch = document.getElementById("textSearch").value;
     let tagSearch = document.getElementById("tagSearch").value;
 
+
     console.log(tagSearch);
     for(let i=0;i<allSearch.length;i++){
+        // text search
         if(textSearch != ""){
             if(allSearch[i].name.includes(textSearch)){
-                console.log("textsearhc");
-                searchResult.push(allSearch[i])
-                // console.log(searchResult[i]);
+                if(!searchResult.includes(allSearch[i])){
+                    console.log(allSearch[i]);
+                    searchResult.push(allSearch[i])
+                }
             }
         }
 
-        if(tagSearch != ""){
-            if(allSearch[i].tags.includes(tagSearch)){
-                searchResult.push(allSearch[i])
+        // tag search
+        if(tagSearch != "category"){
+
+            // console.log(searchResult.length);
+            if(searchResult.length!=0){
+                if(!searchResult.includes(allSearch[i])){
+                    searchResult.push(allSearch[i])
+                }
+                for(let j=0;j<searchResult.length;j++){
+                    if(!searchResult[j].tags.includes(tagSearch)){
+                        searchResult.splice(j,1)
+                    }
+                }
+
+            }else{
+
+                if(allSearch[i].tags.includes(tagSearch)){
+                    if(!searchResult.includes(allSearch[i])){
+                        searchResult.push(allSearch[i])
+                    }
+                }
             }
         }
+
+        // TODO: location search and date/time search
+
 
     }
-    console.log(searchResult);
+    // console.log(searchResult);
     for(let i=0;i<searchResult.length;i++){
         displayResult(searchResult[i])
     }
 })
-// 	document.querySelectorAll(".cloned-events-container").forEach(event => {
-// 		event.replaceChildren();
-// 	});
-//     const searchSnapshot = await getDocs(e);
-//     searchSnapshot.forEach((doc)=>{
-//         console.log(doc.data());
-//         searchResult.push(doc.data())
-//     }
-// })
 
 
-
-    // e = query(collection(dbf.db, "events"),where("name", ">=", textSearch.value.toString())
-    //                                       ,where("name", "<=", textSearch.value.toString()+'\uf8ff'))
-
-    // console.log(doc.data().coverImage);
-    // let eventBlock = document.querySelector(".event-container");
-    // let clonedEvent = eventBlock.cloneNode(true);
-    // clonedEvent.removeAttribute("hidden");
-    // eventBlock.parentNode.insertBefore(clonedEvent, eventBlock.nextSibling);
-    // clonedEvent.querySelector(".card-content.image.search-page").src = doc.data().coverImage;
-    // clonedEvent.querySelector(".card-content.event-name").innerHTML = doc.data().name;
-    // // clonedEvent.querySelector(".card-content.location").innerHTML = doc.data().name;
-
-    // function reverseGeo(l){
-    //     tt.services.reverseGeocode({
-    //     key: tomtomApiKey,
-    //     position: l
-    // })
-    // .then(result=>{
-    //     clonedEvent.querySelector(".card-content.location").innerHTML = result.addresses[0].address.freeformAddress
-    //     // console.log(result.addresses[0].address.freeformAddress);
-    // });
-    // }
-    // reverseGeo(doc.data().location)
-
-    // // card-content location
-    // clonedEvent.querySelector(".card-content.time").innerHTML = "not sure yet";
-    // })
-// })
+document.getElementById("resetButton").addEventListener("click",async()=>{
+    document.querySelectorAll(".cloned-events-container").forEach(event => {
+        event.replaceChildren();
+    });
+    for(let i=0;i<allSearch.length;i++){
+        displayResult(allSearch[i])
+    }
+})
