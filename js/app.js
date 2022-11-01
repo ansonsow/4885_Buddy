05a3e124a76b2e517ce5c99cbc6f42aaa20a74f3
vijaykrishna54 <yarabarlavijaykrishna1437@gmail.com>
2022-11-01 15:15:57 -0700

    CreateEvent.js file updated
    Still cannot get output

--- js/app.js
+++ js/app.js
@@ -27,8 +27,11 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app); 
-export const storage = getStorage(app); 
-export const storageRef = ref(storage);//Created a const to store
+export const storage = getStorage(); //Created a const to store
+// export const storageRef = ref(storage, 'some-child');
+// export const toUpload =  uploadBytes(storageRef, ).then((snapshot)=>{
+//   console.log("File Uploaded!");
+// }); //Reference to storage
 export const db = getFirestore();
 export const auth = getAuth(app);
 
--- js/createMyEvent.js
+++ js/createMyEvent.js
@@ -1,46 +1,33 @@
-// Method 1 : 
 
-    //     var uploader   = document.getElementById('upload');
-    //     document.getElementById('filebutton').addEventListener('onclick', function(e){
-    //         alert("Page linked!");
-    //     var file = e.target.files[0];
-    //     var storageRef = firebase.storage().ref('img/'+file.name);
-    //     var task = storageRef.put(file);
-    //     task.on('state_changed', function progress(snapshot) {
-    //       var percentage = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
-    //       uploader.value = percentage;
-      
-    //     }, function error(err) {
-      
-      
-    //     },function complete() {
-      
-    //     });
-    //   }); 
+files.addEventListener("click", async() =>{
 
-// ------------------------------------------
+    const module1 = await import("./app.js");
+    // here access anything exported in module1
+    alert("This function is working!");
 
-// Method 2 : 
-// import {storageRef} from "../app.js";
-
-      //function to save file
-    function uploadFile(){
-      
-        // Created a Storage Reference with root dir
-        var storageRef = firebase.storage().ref();
-        // Get the file from DOM
-        var file = document.getElementById("files").files[0];
-        console.log(file);
         
-        //dynamically set reference to the file name
-        var thisRef = storageRef.child(file.name);
-  
-        //put request upload file to firebase storage
-        thisRef.put(file).then(function(snapshot) {
-           alert("File Uploaded")
-           
-        });
-      }
+
+        const file = document.getElementById("upload").files[0];
+            console.log(file);
+
+        const name = new Date() + '-' + file.name ;
+
+        const metadata = {
+            contentType:file.type
+        }
+
+        const ref = module1.storage.ref(); //The problem is in this line...
+
+        const task = ref.child(name).put(file,metadata);
+        console.log(task);
+        task
+        .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(url => {
+            console.log(url);
+            alert("Image is uploaded successfully!");
+        })
+            
+});
 
 
 
