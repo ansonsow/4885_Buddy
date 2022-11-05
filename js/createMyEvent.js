2b922aa023749a7ca41ac992ce3caa924955ed8b
vijaykrishna54 <yarabarlavijaykrishna1437@gmail.com>
2022-11-04 18:46:09 -0700

    CreateEvent.js
    Function is running now!

--- js/createMyEvent.js
+++ js/createMyEvent.js
@@ -1,148 +1,23 @@
 
-import {StorageRef,toUpload,uploadImage} from './app.js'
-import { getStorage, ref, uploadBytesResumable } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js";
+import {storage} from './app.js'
+import {getStorage,ref, uploadBytes} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js'
 
-
-
-// files.addEventListener("click", async() =>{
+files.addEventListener("click", async() =>{
 
     
-//     alert("This function is working!");
+    alert("This function is working!");
 
-//         const file = document.getElementById("upload").files[0];
-//             console.log(file);
-
-//         const name = new Date() + '-' + file.name ;
-//             console.log(name);
-
-//         const metadata = {
-//             contentType:file.type
-//         }
-//         const storage = getStorage(app); //Created a const to store
-//         const StorageRef = ref(storage);
-
-//         //  = ref('img/'+file.name); //The problem is in this line...
-
-//          const imageUpload = StorageRef.uploadBytes('img/'+file.name).then((snapshot)=>{
-//             console.log("File Uploaded!");
-//           });
-
-//         const task = imageUpload.put(file,metadata);
-//         console.log(task);
-//         task
-//         .then(snapshot => snapshot.ref.getDownloadURL())
-//         .then(url => {
-//             console.log(url);
-//             alert("Image is uploaded successfully!");
-//         })
-            
-// });
-
-
-//For Authentication (not used now!)
-//   var auth = firebase.auth();
-//To create a storage reference
-//   var storageRef = firebase.storage().ref();
-
-//To Handle files
-  
-    document.getElementById('files').addEventListener('click', (e) => {
-        e.preventDefault();
-        // console.log("aaaaaa");
-    // var file = e.target.files[0];
-    // var file = "../images/badges_01.png"
-    // var f = new File([""], "/Application/MAMP/htdocs/4885-Buddys/images/badges_01.png");
-    // console.log(f);
-
-
-    // var metadata = {
-    //   'contentType': file.type
-    // };
-
-    // // Push to child path.
-    // StorageRef.child('images/' + file.name).put(file, metadata).then(function(snapshot) {
-    // //  To check the size of the file
-    // //   console.log('Uploaded', snapshot.totalBytes, 'bytes.');
-    //      console.log('File metadata:', snapshot.metadata);
-    // // To get a download URL for the file.
-    //   snapshot.ref.getDownloadURL().then(function(url) {
-    //     console.log('File is available at : ', url);
-    // });
-    // }).catch(function(error) {
-    //   console.error('Upload failed:', error);
-
-
-
-
-
-
-
-      const file = document.getElementById("upload").files[0];
+        const file = document.getElementById("upload").files[0];
             console.log(file);
 
         const name = new Date() + '-' + file.name ;
             console.log(name);
 
-        const metadata = {
-            contentType:file.type
-        }
-        // const storage = getStorage(app); //Created a const to store
-        // const StorageRef = ref(storage);
+        const storageRef = ref(storage, name);
 
-        //  = ref('img/'+file.name); //The problem is in this line...
-
-        //  const imageUpload = StorageRef.uploadBytes('img/'+file.name).then((snapshot)=>{
-        //     console.log("File Uploaded!");
-        //   });
-
-        const task = StorageRef.uploadBytesResumable(file,metadata);
-        console.log(task);
-        task
-        .then(snapshot => snapshot.ref.getDownloadURL())
-        .then(url => {
-            console.log(url);
-            alert("Image is uploaded successfully!");
-        })
-
-
-    // uploadImage(file);
-
-    });
-  
-    
-
-//For Validating users-------
-
-
-//   window.onload = function() {
-
-//     document.getElementById('files').addEventListener('change', handleFileSelect, false);
-//     //Disabled for un-authorized users to upload images
-//     document.getElementById('file').disabled = true;
-//     auth.onAuthStateChanged(function(user) {
-//       if (user) {
-//         console.log('Anonymous user signed-in.', user);
-//     //Enabled Authorized users to upload images    
-//         document.getElementById('file').disabled = false;
-//       } else {
-//         console.log('There was no anonymous session. Creating a new anonymous user.');
-//         //To Sign the user in anonymously since accessing Storage requires the user to be authorized.
-//         auth.signInAnonymously().catch(function(error) {
-//           if (error.code === 'auth/operation-not-allowed') {
-//             window.alert('Anonymous Sign-in failed. Please make sure that you have enabled anonymous ' +
-//                 'sign-in on your Firebase project.');
-//           }
-//         });
-//       }
-//     });
-//   }
-
-// --------------Ignore the below lines----------------
-// evt.stopPropagation();
-
-//To include the image URL as a link on HTML page (We can do this only if needed!!) -- ignore for now!!
-// document.getElementById('linkbox').innerHTML = '<a href="' +  url + '">Click For File</a>';
-
-
-
+        uploadBytes(storageRef, file).then((snapshot) => {
+    console.log('Uploaded a blob or file!');
+        });
+            
+});
 
